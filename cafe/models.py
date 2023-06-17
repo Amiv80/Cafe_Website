ad15eeaa2f871c574557e34938b6e52d30a9a418
amiv.80 <amir.r.v138@gmail.com>
2023-06-17 10:06:40 +0330

    Improved HTML and CSS styles and formatting

--- cafe/models.py
+++ cafe/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Customer(models.Model):
-    rate_price = models.FloatField()
-    vip_question = models.CharField(max_length=3)
-    vip_discount_rate = models.FloatField()
-    hour = models.FloatField()
-    minute = models.IntegerField()
-    cost = models.FloatField()
-    discount_enabled = models.BooleanField(default=False)
+    rate_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Rate Price')
+    vip_question = models.CharField(max_length=3, verbose_name='VIP Question')
+    vip_discount_rate = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='VIP Discount Rate')
+    time = models.TimeField(verbose_name='Time')
+    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Cost')
+    discount_enabled = models.BooleanField(default=False, verbose_name='Discount Enabled')
